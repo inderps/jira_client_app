b9bf9ca3f937e75e8d8ac7fd9d8192496ca66405
Inderpal Singh <singhinther@gmail.com>
2016-06-10 01:08:39 +0530

    added prod webpack file

---
+++ client/config/webpack.config.prod.js
@@ -0,0 +1,65 @@
+const path = require('path');
+const webpack = require('webpack');
+const autoprefixer = require('autoprefixer');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const PATHS = {
+  app: path.resolve(__dirname, '../app'),
+  build: path.resolve(__dirname, '../../app/assets'),
+};
+
+const plugins = [
+  new webpack.NoErrorsPlugin(),
+  new webpack.optimize.OccurenceOrderPlugin(),
+  new webpack.optimize.DedupePlugin(),
+  new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false,
+    },
+  }),
+  new ExtractTextPlugin('stylesheets/app.css', {
+    allChunks: true,
+  }),
+];
+
+module.exports = {
+  entry: [path.resolve(PATHS.app, 'main.js')],
+  output: {
+    path: PATHS.build + '/javascripts',
+    filename: 'bundle.js',
+    publicPath: '/',
+  },
+  stats: {
+    colors: true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx', '.css'],
+  },
+  module: {
+    preLoaders: [{
+      test: /\.(js|jsx)$/,
+      exclude: /node_modules/,
+      loader: 'eslint-loader',
+    }],
+    loaders: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        loaders: ['react-hot', 'babel'],
+        include: PATHS.app,
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style', 'css', 'postcss-loader'),
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2)$/,
+        loader: 'url-loader?limit=8192',
+      },
+    ],
+  },
+  plugins: plugins,
+  postcss: function () {
+    return [autoprefixer];
+  },
+};
