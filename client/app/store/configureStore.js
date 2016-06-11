31e7a24fb2fdd2852e51a31f3e50c13deb54a6b5
Inderpal Singh <singhinther@gmail.com>
2016-06-11 18:56:00 +0530

    redux thunk for ajax call

--- client/app/store/configureStore.js
+++ client/app/store/configureStore.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import issueReducer from '../reducers/issueReducer';
 
 export default function configureStore(initialState) {
-  const store = createStore(issueReducer, initialState);
+  const store = createStore(issueReducer, initialState, applyMiddleware(thunk));
   if (module.hot) {
     module.hot.accept('../reducers/issueReducer', () => {
       const nextRootReducer = require('../reducers/issueReducer');
