c884906c861af9724f0f5a624da6d022cb75fbc9
Inderpal Singh <singhinther@gmail.com>
2016-06-12 09:38:31 +0530

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :auto_login_test_user
 
+  # as there is no authentication in place, I'm assuming test user is already logged in
   def auto_login_test_user
     @current_user ||= User.find_by_username('test_user')
   end
